2007b5b207582d3f1509319c9c34fc456f43bc7f
Graziano Statello <graziano.statello@signal-ai.com>
2022-04-04 14:45:51 +0100

    refactor, reuse internal functions

--- src/logic/game.js
+++ src/logic/game.js
@@ -1,8 +1,8 @@
 import * as R from 'ramda';
 
 const mapI = R.addIndex(R.map);
-
-export const sweep = () => {};
+const increase = R.add(1);
+const decrease = R.subtract(R.__, 1);
 
 const createMineField = ({ width = 8, height = 8 }) =>
   R.map(() => R.map(() => false, [...new Array(width)]), [...new Array(height)]);
@@ -15,49 +15,38 @@ const mapDefaultArgs = ({ width = 8, height = 8, mines = 10 } = defaultEasy) => 
   mines,
 });
 
-const addFlagToRow = (x, row) => mapI((tile, j) => (j === x ? 'flag' : tile), row);
-const addFlagToField = (x, y, field) =>
-  mapI((row, i) => (i === y ? addFlagToRow(x, row) : row), field);
-
-const removeFlagFromRow = (x, row) => mapI((tile, j) => (j === x ? false : tile), row);
-const removeFlagFromField = (x, y, field) =>
-  mapI((row, i) => (i === y ? removeFlagFromRow(x, row) : row), field);
-
-const checkTile =
-  (arg) =>
-  (x, y) =>
-  ([field]) =>
-    field[y][x] === arg;
-
-const hasFlag = checkTile('flag');
-const isUnchecked = checkTile(false);
+const checkTile = R.curryN(4)((arg, x, y, [field]) => field[y][x] === arg);
+const updateRowTile = (arg, x, row) => mapI((tile, j) => (j === x ? arg : tile), row);
+const updateTile = R.curryN(4)((arg, x, y, field) =>
+  mapI((row, i) => (i === y ? updateRowTile(arg, x, row) : row), field)
+);
 
 const removeFlagAndAddMine =
   (x, y) =>
   ([field, mines]) =>
-    [removeFlagFromField(x, y, field), mines + 1];
+    [updateTile(false, x, y)(field), increase(mines)];
 
 const addFlagAndRemoveMine =
   (x, y) =>
   ([field, mines]) =>
-    [addFlagToField(x, y, field), mines - 1];
+    [updateTile('flag', x, y, field), decrease(mines)];
 
 export const removeFlag = (x, y) =>
   R.cond([
-    [hasFlag(x, y), removeFlagAndAddMine(x, y)],
+    [checkTile('flag', x, y), removeFlagAndAddMine(x, y)],
     [R.always, R.identity],
   ]);
 
 export const addFlag = (x, y) =>
   R.cond([
-    [isUnchecked(x, y), addFlagAndRemoveMine(x, y)],
-    [hasFlag(x, y), R.identity],
+    [checkTile(false, x, y), addFlagAndRemoveMine(x, y)],
+    [checkTile('flag', x, y), R.identity],
   ]);
 
 export const flag = (x, y) =>
   R.cond([
-    [isUnchecked(x, y), addFlagAndRemoveMine(x, y)],
-    [hasFlag(x, y), removeFlagAndAddMine(x, y)],
+    [checkTile(false, x, y), addFlagAndRemoveMine(x, y)],
+    [checkTile('flag', x, y), removeFlagAndAddMine(x, y)],
     [R.always, R.identity],
   ]);
 
@@ -66,3 +55,5 @@ export const start = R.pipe(
   (args) => [createMineField(args), args],
   ([minefield, { mines }]) => [minefield, mines]
 );
+
+export const sweep = () => {};
